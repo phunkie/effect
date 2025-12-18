48e5e0c69ae9d674577f998e086b2e465c90d408
Marcello Duarte <marcello.duarte@gmail.com>
2025-12-18 00:00:58 +0000

    docs: add copyright and class documentation to IOApp classes

--- src/IO/IOApp/OptionDefinition.php
+++ src/IO/IOApp/OptionDefinition.php
@@ -1,7 +1,19 @@
 <?php
 
+/*
+ * This file is part of Phunkie Effect, A functional effect system for PHP inspired by Cats Effect.
+ *
+ * (c) Marcello Duarte <marcello.duarte@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Phunkie\Effect\IO\IOApp;
 
+/**
+ * Defines a command line option.
+ */
 class OptionDefinition
 {
     public readonly ?string $short;
--- src/IO/IOApp/ParsedOptions.php
+++ src/IO/IOApp/ParsedOptions.php
@@ -1,9 +1,21 @@
 <?php
 
+/*
+ * This file is part of Phunkie Effect, A functional effect system for PHP inspired by Cats Effect.
+ *
+ * (c) Marcello Duarte <marcello.duarte@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Phunkie\Effect\IO\IOApp;
 
 use Phunkie\Types\Either;
 
+/**
+ * Result of the options parsing process.
+ */
 class ParsedOptions
 {
     /**
